a1305a70583314469763c744fb1dfa4eb41b1528
JørR <43743804+Ezyrone@users.noreply.github.com>
2024-10-28 17:13:48 +0100

    refactoring code

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 // Created by Jory Grzeszczak on 28/10/2024.
 //
 
-
 #include <stdio.h>
 
 // Function to find the maximum value in an array
@@ -19,22 +18,18 @@ int findMax(int arr[], int size) {
 int main() {
     int n;
 
-    // Ask the user for the size of the array
     printf("Enter the number of elements in the array: ");
     scanf("%d", &n);
 
     int arr[n];
 
-    // Take input for the array elements
     printf("Enter the elements of the array:\n");
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Find the maximum value using the function
     int max = findMax(arr, n);
 
-    // Display the maximum value
     printf("The maximum value in the array is: %d\n", max);
 
     return 0;
