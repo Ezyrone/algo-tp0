e1e5eacbfbd1bc28b12bd82083a59e559b735c13
JørR <43743804+Ezyrone@users.noreply.github.com>
2024-10-28 17:11:17 +0100

    Create programm for the 1st exercice

--- main.c
+++ main.c
@@ -1,3 +1,41 @@
 //
 // Created by Jory Grzeszczak on 28/10/2024.
 //
+
+
+#include <stdio.h>
+
+// Function to find the maximum value in an array
+int findMax(int arr[], int size) {
+    int max = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int n;
+
+    // Ask the user for the size of the array
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Take input for the array elements
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Find the maximum value using the function
+    int max = findMax(arr, n);
+
+    // Display the maximum value
+    printf("The maximum value in the array is: %d\n", max);
+
+    return 0;
+}
